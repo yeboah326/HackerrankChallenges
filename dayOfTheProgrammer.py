a411b8532577c28441a09b18d719fdc69b34361e
yeboah326 <asanteg36@gmail.com>
2019-11-13 11:07:16 +0000

    modified the day of programmer challenge

--- dayOfTheProgrammer.py
+++ dayOfTheProgrammer.py
@@ -5,11 +5,11 @@ def isJulianLeap(yyyy):
     return "Leap" if yyyy % 4 == 0 else "Normal"
 
 def isGreogrianLeap(yyyy):
-    return "Leaap" if (yyyy % 400 == 0) or ((yyyy % 4 == 0) and (yyyy % 100 != 0)) else "Normal"
+    return "Leap" if (yyyy % 400 == 0) or ((yyyy % 4 == 0) and (yyyy % 100 != 0)) else "Normal"
 
 def dayOfProgrammer(yyyy):
     if yyyy == 1918:
-        return "12.09.1918"
+        return "26.09.1918"
     elif 1700 <= yyyy <= 1917:
         return "12.09." + str(yyyy) if isJulianLeap(yyyy) == "Leap" else ("13.09." + str(yyyy))
     elif 1919 <= yyyy <= 2700:
